4cd0a8d7b643dba4799ea21429c4e39bdecaaad0
Igor Varshavsky <igor.varshavsky@gmail.com>
2021-07-17 00:29:35 -0400

    Added Beer Controller. Closes #3

---
+++ src/main/java/ca/iva/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,34 @@
+package ca.iva.msscbeerservice.web.controller;
+
+import ca.iva.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * Created By iVa on 7/16/2021.
+ */
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+//todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+        //todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+//todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
